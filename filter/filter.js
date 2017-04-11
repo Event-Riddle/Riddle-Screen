820be85543360a0ac1c158ab31e61b3b89123526
Babis86 <babisgeorgiadis@hotmail.de>
2017-04-11 21:47:27 +0200

    test

--- filter/filter.js
+++ filter/filter.js
@@ -17,13 +17,22 @@ var  common_options_init = null;
     common_options_init.forEach(function myFunction(common_options, index) {
       if( (common_options['name'] !== '' || common_options['name'] !== undefined) && common_options['name']) {
         console.log('name ok');
-        if (common_options['active'] === true) {
+        if (common_options['active'] === true) { console.log('active ok');
+        console.log(common_options['threshold-value-top']);
+        console.log(common_options['threshold-value-bottom']);
+        console.log((common_options['threshold-value-bottom'] === '' || common_options['threshold-value-bottom'] === undefined) );
+        console.log((common_options['threshold-value-top'] !== '' || common_options['threshold-value-top'] !== undefined));
           if((common_options['threshold-value-bottom'] === '' || common_options['threshold-value-bottom'] === undefined) && (common_options['threshold-value-top'] !== '' || common_options['threshold-value-top'] !== undefined)){
+            console.log('botoom nix drin');;
+            console.log(incoming[common_options['filter-top-id']]);
+            console.log(common_options['threshold-value-top']);
              if (incoming[common_options['filter-top-id']] == common_options['threshold-value-top']) {
                    filtered = true;
              }
            } else if((common_options['threshold-value-bottom'] !== '' || common_options['threshold-value-bottom'] !== undefined) && (common_options['threshold-value-top'] !== '' || common_options['threshold-value-top'] !== undefined)) {
               if (incoming[common_options['filter-top-id']] <= common_options['threshold-value-top'] && incoming[common_options['filter-top-id']] >= common_options['threshold-value-bottom']){
+                console.log('range between: ' + common_options['threshold-value-top'] + ' and ' + common_options['threshold-value-bottom']);
+                console.log('event value: ' + incoming[common_options['filter-top-id']]);
                 filtered = true;
               }
            }
