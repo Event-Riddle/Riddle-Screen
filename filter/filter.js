c9d9a9bd8b91b053bc1ef1232891195f29e4703e
Babis86 <babisgeorgiadis@hotmail.de>
2017-04-11 20:50:40 +0200

    bugfix

--- filter/filter.js
+++ filter/filter.js
@@ -15,13 +15,13 @@ var  common_options_init = null;
           var filtered = false;
 
     common_options_init.forEach(function myFunction(common_options, index) {
-      if(typeof common_options['name'] !== '' && common_options['name']) {
+      if( (common_options['name'] !== '' || common_options['name'] !== 'undefined') && common_options['name']) {
         if (common_options['active'] === true) {
-          if(common_options['threshold-value-bottom'] === '' && common_options['threshold-value-top'] !== ''){
+          if((common_options['threshold-value-bottom'] === '' || common_options['threshold-value-bottom'] === 'undefined') && (common_options['threshold-value-top'] !== '' || common_options['threshold-value-top'] != 'undefined')){
              if (incoming[common_options['filter-top-id']] === common_options['threshold-value-top']) {
                    filtered = true;
              }
-           } else if(common_options['threshold-value-bottom'] !== '' && common_options['threshold-value-top'] !== '') {
+           } else if((common_options['threshold-value-bottom'] !== '' || common_options['threshold-value-bottom'] !== 'undefined') && (common_options['threshold-value-top'] !== '' || common_options['threshold-value-top'] !== 'undefined')) {
               if (incoming[common_options['filter-top-id']] <= common_options['threshold-value-top'] && incoming[common_options['filter-top-id']] >= common_options['threshold-value-bottom']){
                 console.log('range between: ' + common_options['threshold-value-top'] + ' and ' + common_options['threshold-value-bottom']);
                 console.log('event value: ' + incoming[common_options['filter-top-id']]);
